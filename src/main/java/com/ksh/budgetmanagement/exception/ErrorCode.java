e053612ef8e66020349bf594a0461261027a31fa
kimsungho <tigertjdgh@gmail.com>
2024-11-18 00:13:05 +0900

    에러 코드 추가

--- src/main/java/com/ksh/budgetmanagement/exception/ErrorCode.java
+++ src/main/java/com/ksh/budgetmanagement/exception/ErrorCode.java
@@ -9,7 +9,9 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    USER_NOT_FOUNT("회원을 찾을 수 없습니다.", NOT_FOUND);
+    USER_NOT_FOUNT("회원을 찾을 수 없습니다.", NOT_FOUND),
+    ALREADY_USED_EMAIL("이미 사용중인 이메일입니다.", HttpStatus.BAD_REQUEST),
+    ALREADY_USED_NICKNAME("이미 사용중인 닉네임입니다.", HttpStatus.BAD_REQUEST);
 
     private final String message;
     private final HttpStatus status;
